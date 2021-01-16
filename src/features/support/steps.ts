5035283e20b28bfbb99e475e14e234604ab3023a
quantumsheep <nathanael.dmc@outlook.fr>
2021-01-16 03:24:36 +0100

    Implement wallet tests

---
+++ src/features/support/steps.ts
@@ -0,0 +1,41 @@
+import { Given, When, Then, DataTable } from '@cucumber/cucumber';
+import { strict as assert } from 'assert';
+import { Stock, WalletWorld } from './world';
+
+interface DataTableGeneric<T> extends DataTable {
+  hashes(): T[];
+}
+
+Given('a wallet containing {int} stocks', function (this: WalletWorld, stocks_count: number, stocks: DataTableGeneric<Stock>) {
+  this.setStocks(stocks.hashes());
+});
+
+Given('USD to {string} rate is {float}', function (this: WalletWorld, currency: string, rate: number) {
+  this.setRate(currency, rate);
+});
+
+When('converting to {string}', function (this: WalletWorld, currency: string) {
+  this.convert(currency);
+});
+
+Then('an error is returned', function (this: WalletWorld) {
+  const error = this.getError();
+
+  assert.notEqual(error, null);
+  assert.notEqual(error, undefined);
+});
+
+Then('value should be null', function (this: WalletWorld) {
+  const value = this.getValue();
+  assert.equal(value, null);
+});
+
+Then('error should be null', function (this: WalletWorld) {
+  const error = this.getError();
+  assert.equal(error, null);
+});
+
+Then('value should be {float}', function (this: WalletWorld, expectedValue: number) {
+  const value = this.getValue();
+  assert.equal(value, expectedValue);
+});
